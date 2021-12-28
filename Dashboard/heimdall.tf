49a1a7bfeeb8f45bb5cdf1d38517a23da8d7b905
Shelby M <shelbygt500martin@gmail.com>
2021-12-27 23:40:27 -0500

    added subfolders for different microservices

--- Dashboard/heimdall.tf
+++ Dashboard/heimdall.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
 resource "docker_image" "heimdall" {
   name = "linuxserver/heimdall:latest"
   force_remove = true

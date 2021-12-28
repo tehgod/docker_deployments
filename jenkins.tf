210a12694c7a12ea160f02982a9ad521a660030f
Shelby M <shelbygt500martin@gmail.com>
2021-12-27 21:19:54 -0500

    added jenkins tf file

---
+++ jenkins.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "jenkins" {
+  name = "jenkins/jenkins:latest"
+  force_remove = true
+}
+
+resource "docker_container" "jenkins" {
+  name = "jenkins"
+  image = docker_image.jenkins.latest
+  restart = "unless-stopped"
+  volumes {
+      container_path = "/var/jenkins_home"
+      host_path = "${pathexpand("~")}/config/app_config/jenkins"
+  }
+  ports {
+        internal = 8080
+        external = 8080
+    }
+    ports {
+        internal = 50000
+        external = 50000
+    }
+}

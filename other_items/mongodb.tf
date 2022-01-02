8d873010c160cde38b6cc6228f2ff86fafbb10e8
Shelby M <shelbygt500martin@gmail.com>
2022-01-02 01:09:25 -0500

    mongo tf added

--- other_items/mongodb.tf
+++ other_items/mongodb.tf
@@ -1,5 +1,5 @@
 resource "docker_image" "mongo" {
-  name = "mongo:latest"
+  name = "mongo:4.4.11"
   force_remove = true
 }
 
@@ -7,6 +7,7 @@ resource "docker_container" "mongo" {
     name = "mongo"
     image = docker_image.mongo.latest
     restart = "always"
+    rm = true
     ports {
         internal = 27017
         external = 27017

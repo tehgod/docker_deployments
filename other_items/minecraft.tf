d659fd19bb67b57a48c31cbcd5dde57ac1cc418b
Shelby M <shelbygt500martin@gmail.com>
2022-01-02 20:22:01 -0500

    minecraft container attempt 1

---
+++ other_items/minecraft.tf
@@ -0,0 +1,23 @@
+resource "docker_image" "minecraft" {
+  name = "itzg/minecraft:latest"
+  force_remove = true
+}
+
+resource "docker_container" "minecraft" {
+    name = "MC Server ${var.server_name}"
+    image = docker_image.minecraft.latest
+    restart = "always"
+    ports {
+        internal = 25565
+        external = 25565
+    }
+    env = [
+        "EULA=TRUE",
+        "DIFFICULTY=hard",
+        "SERVER_NAME=${var.server_name}"
+    ]
+    volumes {
+        container_path = "/data"
+        host_path = "${pathexpand("~")}/config/app_config/minecraft/${var.server_name}"
+    }
+}

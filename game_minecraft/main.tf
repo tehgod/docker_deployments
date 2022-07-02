019152c9adee1010cb9c7d6c5b5674459ceed713
Shelby <shelbygt500martin@gmail.com>
2022-07-02 15:43:43 -0400

    converted mc server tf

---
+++ game_minecraft/main.tf
@@ -0,0 +1,21 @@
+locals {
+  server_name = ""
+}
+
+module "minecraft" {
+    source = "./modules"
+    container_name = "mc_server_${local.server_name}"
+    resource_location = "itzg/minecraft-server:latest"
+    env = [
+        "EULA=TRUE",
+        "DIFFICULTY=hard",
+        "SERVER_NAME=${local.server_name}",
+	    "HARDCORE=true"
+    ]
+    docker_ports = [
+        {"internal":25565, "external":25565, "protocol":"tcp"}
+    ]
+    docker_volumes = [
+        {"host_path":"${pathexpand("~")}/config/app_config/minecraft/${local.server_name}", "container_path":"/data"}
+    ]
+}
